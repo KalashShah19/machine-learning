e49ba3ab9df66f2f24050e2bd9007a1649591390
KalashShah19 <92210070+KalashShah19@users.noreply.github.com>
2024-06-27 12:00:46 +0530

    27 / 06
    multiple linear regression, decision tree, random forest

---
+++ Practical/decision_tree_regression.py
@@ -0,0 +1,39 @@
+# Decision Tree Regression
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Position_Salaries.csv')
+X = dataset.iloc[:, 1:-1].values
+y = dataset.iloc[:, -1].values
+
+# Training the Decision Tree Regression model on the whole dataset
+from sklearn.tree import DecisionTreeRegressor
+regressor = DecisionTreeRegressor(random_state = 0)
+regressor.fit(X, y)
+
+# Predicting a new result
+regressor.predict([[6.5]])
+
+from sklearn.metrics import r2_score, mean_squared_error
+# Calculate and print R^2 score (optional)
+y_pred = regressor.predict(X)
+r2 = r2_score(y, y_pred)
+print(f'R^2 score: {r2:.2f}')
+
+# Calculate and print mean squared error (MSE)
+mse = mean_squared_error(y, y_pred)
+print(f'Mean Squared Error: {mse:.2f}')
+
+# Visualising the Decision Tree Regression results (higher resolution)
+X_grid = np.arange(min(X), max(X), 0.01)
+X_grid = X_grid.reshape((len(X_grid), 1))
+plt.scatter(X, y, color = 'red')
+plt.plot(X_grid, regressor.predict(X_grid), color = 'blue')
+plt.title('Truth or Bluff (Decision Tree Regression)')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()
---
+++ Practical/random_forest_regression.py
@@ -0,0 +1,39 @@
+# Random Forest Regression
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Position_Salaries.csv')
+X = dataset.iloc[:, 1:-1].values
+y = dataset.iloc[:, -1].values
+
+# Training the Random Forest Regression model on the whole dataset
+from sklearn.ensemble import RandomForestRegressor
+regressor = RandomForestRegressor(n_estimators = 10, random_state = 0)
+regressor.fit(X, y)
+
+# Predicting a new result
+regressor.predict([[6.5]])
+
+from sklearn.metrics import r2_score, mean_squared_error
+# Calculate and print R^2 score (optional)
+y_pred = regressor.predict(X)
+r2 = r2_score(y, y_pred)
+print(f'R^2 score: {r2:.2f}')
+
+# Calculate and print mean squared error (MSE)
+mse = mean_squared_error(y, y_pred)
+print(f'Mean Squared Error: {mse:.2f}')
+
+# Visualising the Random Forest Regression results (higher resolution)
+X_grid = np.arange(min(X), max(X), 0.01)
+X_grid = X_grid.reshape((len(X_grid), 1))
+plt.scatter(X, y, color = 'red')
+plt.plot(X_grid, regressor.predict(X_grid), color = 'blue')
+plt.title('Truth or Bluff (Random Forest Regression)')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()
